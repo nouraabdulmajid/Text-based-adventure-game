9adbd85bac319c32b60dfc3b8def034547da2f5d
nouraabdulmajid <nouraamajid@hotmail.com>
2024-10-31 11:59:11 +0000

    Add files via upload

--- IndividualProject/src/fileSaver/FileSaver.java
+++ IndividualProject/src/fileSaver/FileSaver.java
@@ -1,25 +1,24 @@
-/*
+
 package fileSaver;
 
-import domain.City;
-
+import application.GameLogic;
+import domain.*;
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.util.ArrayList;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class FileSaver {
-    private FileWriter writer;
-    private Scanner reader;
 
     public FileSaver(String filename) {
-        this.writer = new FileWriter(filename, true);
-        this.reader = new Scanner(System.in);
+
 
     }
 
-    public void writeToFile(String name, int funds, ArrayList destinationsVisited) {
+    public void writeToFile(String name, int funds, ArrayList<String> destinationsVisited) {
         // Saving the time
         LocalDateTime currentTime = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
@@ -28,54 +27,70 @@ public class FileSaver {
         String formattedFunds = Integer.toString(funds);
         // Saving the destinations visited
         String formattedDestinations = "";
-        for (City c: destinationsVisited) {
+        for (String city: (ArrayList<String>) destinationsVisited) {
             if (formattedDestinations.length() > 0) {
-                formattedDestinations += " " + c.getName();
+                formattedDestinations += " " + city;
             } else {
-                formattedDestinations += c.getName();
+                formattedDestinations += city;
             }
-
         }
         String saveString = name + ";" + formattedTime + ";" + formattedFunds + ";" + formattedDestinations;
-        this.writer.write(saveString);
-        this.writer.close();
 
+        try {
+            FileWriter writer = new FileWriter("scoreboard.txt");
+            writer.write(saveString);
+            writer.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found.");
+        } catch (Exception c) {
+            System.out.println("An error has occurred.");
+        }
 
     }
 
     public String readFromFile() {
         String outputString = "";
-        while (reader.hasNextLine()) {
-            String currentString = reader.nextLine();
-            String[] parts = currentString.split(";");
-            String name = parts[0];
-            String time = parts[1];
-            String funds = parts[2];
-            String destinations = parts[3];
-            String[] destinationsList = destinations.split(" ");
-            int destinationCount = 0;
-            String destinationPrintout = "";
-            for (String d : destinationsList) {
-                destinationCount += 1;
-                if (destinationCount <= 1) {
-                    destinationPrintout += d;
-                } else if (destinationCount > 1) {
-                    destinationPrintout += ", " + d;
+        try {
+            File scoreboardFile = new File("scoreboard.txt");
+            Scanner reader = new Scanner(scoreboardFile);
+            while (reader.hasNextLine()) {
+                String currentString = reader.nextLine();
+
+                //Breaks down each line on the string into save components
+                String[] parts = currentString.split(";");
+                String name = parts[0];
+                String time = parts[1];
+                String funds = parts[2];
+                String destinations = parts[3];
+
+                //Further breaks down the destinations section into individual destinations
+                String[] destinationsList = destinations.split(" ");
+                int destinationCount = 0;
+                String destinationPrintout = "";
+                for (String d : destinationsList) {
+                    destinationCount += 1;
+                    if (destinationCount <= 1) {
+                        destinationPrintout += d;
+                    } else if (destinationCount > 1) {
+                        destinationPrintout += ", " + d;
+                    }
+                }
+
+                if (destinationCount == 0) {
+                    outputString += name + ", " + "£" + funds + ", " + Integer.toString(destinationCount) + " cities. Save time: " + time + "\n";
+                } else if (destinationCount > 0) {
+                    outputString += name + ", " + "£" + funds + ", " + Integer.toString(destinationCount) + " cities: " + destinationPrintout + ". Save time: " + time + "\n";
                 }
             }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found. ");
 
-            if (destinationCount == 0) {
-                outputString += name + ", " + "£" + funds + ", " + Integer.toString(destinationCount) + " cities. Save time: " + time + "\n";
-            } else if (destinationCount > 0) {
-                outputString += name + ", " + "£" + funds + ", " + Integer.toString(destinationCount) + " cities: " + destinationPrintout + ". Save time: " + time + "\n";
-            }
-            return outputString;
+        } catch (Exception c) {
+            System.out.println("An error has occurred. ");
         }
-
-
-
-
+        return outputString;
     }
 }
 
-*/
+
